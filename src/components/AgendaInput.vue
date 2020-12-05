d565ff047f348eaf4a006cb05da19dd0a93d53b5
Gustaf Bagus Alamsyah <gustafkonamic@gmail.com>
2020-12-05 13:48:12 +0700

    tugasmakanan

---
+++ src/components/AgendaInput.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">List Pesanan</h3>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="">Nama</label>
+                    <input type="text" v-model="agenda.nama" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Makanan</label>
+                    <input type="text" v-model="agenda.makanan" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Minuman</label>
+                    <input type="text" v-model="agenda.minuman" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-danger btn-sm" :disabled="isLoading" @click.prevent="simpan">{{ isLoading ? 'Loading...':'Tambahkan' }}</button>
+                </div>    
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AgendaInput',
+    data(){
+        return {
+            agenda: {
+                nama: '',
+                makanan: '',
+                minuman: ''
+            }
+        }
+    },
+    methods: {
+        simpan(){
+            this.$store.dispatch('simpanAgenda', this.agenda)
+            this.agenda = {
+                nama: '',
+                makanan: '',
+                minuman: ''
+            }
+        }
+    },
+    computed: {
+        isLoading(){
+            return this.$store.state.isLoading
+        }
+    }
+}
+</script>
---
+++ src/components/AgendaShow.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Daftar Pesanan</h3>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Makanan</th>
+                                <th>Minuman</th>
+                                
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(row, index) in agenda" :key="index">
+                                <td>{{ index+1 }}</td>
+                                <td>{{ row.nama }}</td>
+                                <td>{{ row.makanan }}</td>
+                                <td>{{ row.minuman }}</td>
+                                
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AgendaShow',
+        computed: {
+            agenda(){
+                return this.$store.state.listAgenda
+            }
+        }
+    }
+</script>
